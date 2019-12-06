1c3344db5a72c14d771529b9a9a4f6773b5563bd
liws0 <liws0@yonyou.com>
2019-12-06 10:39:42 +0800

    1

--- src/main/java/org/liws/zk/jute/model/MockReqHeader.java
+++ src/main/java/org/liws/zk/jute/model/MockReqHeader.java
@@ -4,9 +4,16 @@ import org.apache.jute.InputArchive;
 import org.apache.jute.OutputArchive;
 import org.apache.jute.Record;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * MockReqHeader表示一个简单的请求头类，通过实现Record来支持序列化和反序列化
  */
+@Setter
+@Getter
+@EqualsAndHashCode
 public class MockReqHeader implements Record {
 	
 	private long sessionId;
@@ -17,19 +24,6 @@ public class MockReqHeader implements Record {
 	public MockReqHeader(long sessionId, String type) {
 		this.sessionId = sessionId;
 		this.type = type;
-	}
-
-	public long getSessionId() {
-		return sessionId;
-	}
-	public void setSessionId(long sessionId) {
-		this.sessionId = sessionId;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String m_) {
-		type = m_;
 	}
 
 	@Override
@@ -50,33 +44,5 @@ public class MockReqHeader implements Record {
 		type = inputArchive.readString("type");
 		
 		inputArchive.endRecord(tag);
-	}
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (sessionId ^ (sessionId >>> 32));
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MockReqHeader other = (MockReqHeader) obj;
-		if (sessionId != other.sessionId)
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		return true;
 	}
 }
